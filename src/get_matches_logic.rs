91706e084e2832ecdfac684886a9e27c56916829
Ben Bicknell <benabicknell@gmail.com>
2023-02-21 13:39:50 -0500

    reorganize

--- src/get_matches_logic.rs
+++ src/get_matches_logic.rs
@@ -1,4 +1,3 @@
-extern crate serde;
 use chrono::{
     format::{DelayedFormat, StrftimeItems},
     prelude::*,
@@ -19,19 +18,19 @@ pub struct Root {
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Event {
-    pub away_score: Score,
+    // pub away_score: Score,
     pub away_team: Team,
-    pub changes: Changes,
+    // pub changes: Changes,
     pub custom_id: String,
     pub final_result_only: bool,
     pub first_to_serve: Option<i64>,
     pub has_global_highlights: bool,
-    pub home_score: Score,
+    //pub home_score: Score,
     pub home_team: Team,
     pub id: Option<i64>,
     pub last_period: Option<String>,
-    pub periods: Periods,
-    pub round_info: Option<RoundInfo>,
+    //  pub periods: Periods,
+    // pub round_info: Option<RoundInfo>,
     pub slug: String,
     pub start_timestamp: Option<i64>,
     pub status: Status,
@@ -42,170 +41,18 @@ pub struct Event {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Score {
-    pub current: Option<i64>,
-    pub display: Option<i64>,
-    pub normaltime: Option<Value>,
-    pub period1: Option<i64>,
-    #[serde(rename = "period1TieBreak")]
-    pub period1tie_break: Option<i64>,
-    pub period2: Option<i64>,
-    #[serde(rename = "period2TieBreak")]
-    pub period2tie_break: Option<i64>,
-    pub period3: Option<i64>,
-    #[serde(rename = "period3TieBreak")]
-    pub period3tie_break: Option<i64>,
-    pub period4: Option<i64>,
-    pub period5: Option<i64>,
-    #[serde(rename = "period5TieBreak")]
-    pub period5tie_break: Option<i64>,
-    pub point: Option<String>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Team {
     pub disabled: Option<Value>,
     pub id: Option<i64>,
     pub name: String,
     pub short_name: String,
     pub slug: String,
-    pub sport: Sport,
-    pub sub_teams: Option<Vec<SubTeam>>,
-    pub team_colors: TeamColors2,
+    //   pub sport: Sport,
+    //   pub sub_teams: Option<Vec<SubTeam>>,
+    //   pub team_colors: TeamColors2,
     #[serde(rename = "type")]
     pub type_field: Option<i64>,
     pub user_count: Option<i64>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Sport {
-    pub id: Option<i64>,
-    pub name: String,
-    pub slug: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct SubTeam {
-    pub disabled: Option<Value>,
-    pub gender: Option<String>,
-    pub id: Option<i64>,
-    pub name: String,
-    pub name_code: Option<String>,
-    pub national: Option<Value>,
-    pub ranking: Option<i64>,
-    pub short_name: String,
-    pub slug: String,
-    pub sport: Sport2,
-    pub team_colors: TeamColors,
-    #[serde(rename = "type")]
-    pub type_field: Option<i64>,
-    pub user_count: Option<i64>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Sport2 {
-    pub id: Option<i64>,
-    pub name: String,
-    pub slug: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct TeamColors {
-    pub primary: String,
-    pub secondary: String,
-    pub text: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct TeamColors2 {
-    pub primary: String,
-    pub secondary: String,
-    pub text: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Changes {
-    pub change_timestamp: Option<i64>,
-    pub changes: Option<Vec<String>>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Sport3 {
-    pub id: Option<i64>,
-    pub name: String,
-    pub slug: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct SubTeam2 {
-    pub disabled: Value,
-    pub gender: String,
-    pub id: Option<i64>,
-    pub name: String,
-    pub name_code: String,
-    pub national: bool,
-    pub ranking: Option<i64>,
-    pub short_name: String,
-    pub slug: String,
-    pub sport: Sport4,
-    pub sub_teams: Option<Vec<Value>>,
-    pub team_colors: TeamColors3,
-    #[serde(rename = "type")]
-    pub type_field: Option<i64>,
-    pub user_count: Option<i64>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Sport4 {
-    pub id: Option<i64>,
-    pub name: String,
-    pub slug: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct TeamColors3 {
-    pub primary: String,
-    pub secondary: String,
-    pub text: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct TeamColors4 {
-    pub primary: String,
-    pub secondary: String,
-    pub text: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Periods {
-    pub current: String,
-    pub period1: String,
-    pub period2: String,
-    pub period3: String,
-    pub period4: String,
-    pub period5: String,
-    pub point: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct RoundInfo {
-    pub cup_round_type: Option<i64>,
-    pub name: String,
-    pub round: Option<i64>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -241,46 +88,19 @@ pub struct Category {
     pub id: Option<i64>,
     pub name: String,
     pub slug: String,
-    pub sport: Sport5,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Sport5 {
-    pub id: Option<i64>,
-    pub name: String,
-    pub slug: String,
+    //  pub sport: Sport5,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct UniqueTournament {
-    pub category: Category2,
+    // pub category: Category2,
     pub has_event_player_statistics: bool,
     pub has_position_graph: Option<String>,
     pub id: Option<i64>,
     pub name: String,
     pub slug: String,
     pub user_count: Option<i64>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Category2 {
-    pub flag: String,
-    pub id: Option<i64>,
-    pub name: String,
-    pub slug: String,
-    pub sport: Sport6,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Sport6 {
-    pub id: Option<i64>,
-    pub name: String,
-    pub slug: String,
-    pub has_position_graph: Option<String>,
 }
 
 impl fmt::Display for Team {
@@ -430,7 +250,6 @@ pub async fn send_today_schedule(
     let dt_for_api: DelayedFormat<StrftimeItems> = get_today().format("%d/%m/%Y");
     const SCHED_URL: &str = "https://tennisapi1.p.rapidapi.com/api/tennis/events/";
     let url: String = format!("{}{}?rapidapi-key={}", SCHED_URL, dt_for_api, api_key);
-    println!("{}", url);
     let request: Request = client.get(url).build().unwrap();
     let resp: Root = client.execute(request).await?.json::<Root>().await?;
     let match_results: String = get_todays_matches(resp.events);
---
+++ src/structs_list_unused.rs
@@ -0,0 +1,178 @@
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Score {
+//     pub current: Option<i64>,
+//     pub display: Option<i64>,
+//     pub normaltime: Option<Value>,
+//     pub period1: Option<i64>,
+//     #[serde(rename = "period1TieBreak")]
+//     pub period1tie_break: Option<i64>,
+//     pub period2: Option<i64>,
+//     #[serde(rename = "period2TieBreak")]
+//     pub period2tie_break: Option<i64>,
+//     pub period3: Option<i64>,
+//     #[serde(rename = "period3TieBreak")]
+//     pub period3tie_break: Option<i64>,
+//     pub period4: Option<i64>,
+//     pub period5: Option<i64>,
+//     #[serde(rename = "period5TieBreak")]
+//     pub period5tie_break: Option<i64>,
+//     pub point: Option<String>,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Sport {
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub slug: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct SubTeam {
+//     pub disabled: Option<Value>,
+//     pub gender: Option<String>,
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub name_code: Option<String>,
+//     pub national: Option<Value>,
+//     pub ranking: Option<i64>,
+//     pub short_name: String,
+//     pub slug: String,
+//     pub sport: Sport2,
+//     pub team_colors: TeamColors,
+//     #[serde(rename = "type")]
+//     pub type_field: Option<i64>,
+//     pub user_count: Option<i64>,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Sport2 {
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub slug: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct TeamColors {
+//     pub primary: String,
+//     pub secondary: String,
+//     pub text: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct TeamColors2 {
+//     pub primary: String,
+//     pub secondary: String,
+//     pub text: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Changes {
+//     pub change_timestamp: Option<i64>,
+//     pub changes: Option<Vec<String>>,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Sport3 {
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub slug: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct SubTeam2 {
+//     pub disabled: Value,
+//     pub gender: String,
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub name_code: String,
+//     pub national: bool,
+//     pub ranking: Option<i64>,
+//     pub short_name: String,
+//     pub slug: String,
+//     pub sport: Sport4,
+//     pub sub_teams: Option<Vec<Value>>,
+//     pub team_colors: TeamColors3,
+//     #[serde(rename = "type")]
+//     pub type_field: Option<i64>,
+//     pub user_count: Option<i64>,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Sport4 {
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub slug: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct TeamColors3 {
+//     pub primary: String,
+//     pub secondary: String,
+//     pub text: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct TeamColors4 {
+//     pub primary: String,
+//     pub secondary: String,
+//     pub text: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Periods {
+//     pub current: String,
+//     pub period1: String,
+//     pub period2: String,
+//     pub period3: String,
+//     pub period4: String,
+//     pub period5: String,
+//     pub point: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct RoundInfo {
+//     pub cup_round_type: Option<i64>,
+//     pub name: String,
+//     pub round: Option<i64>,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Sport5 {
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub slug: String,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Category2 {
+//     pub flag: String,
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub slug: String,
+//     pub sport: Sport6,
+// }
+
+// #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Sport6 {
+//     pub id: Option<i64>,
+//     pub name: String,
+//     pub slug: String,
+//     pub has_position_graph: Option<String>,
+// }
