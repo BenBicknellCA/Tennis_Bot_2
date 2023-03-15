f1e9d1a0fc31899eace3e7f859f5bc81efcb3fe0
Ben Bicknell <benabicknell@gmail.com>
2023-03-15 17:00:50 -0400

    oopsie

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use serenity::{
     prelude::*,
 };
 use std::env;
-
+use std::str::FromStr;
 use tracing::{error, info};
 
 const HELP_MESSAGE: &str = "help message";
@@ -25,9 +25,12 @@ struct Bot {
 
 #[tokio::main]
 async fn main() {
-    let guild_id = GuildId(974015818243375104);
-    let api_key = "a315a7e08fmsh8614ab90179f23fp1a6568jsn46d5a87a2dd8";
-    let token_id = "MTA0NzY3MDgyNjA5MjIxNjM1MA.Gc2mnq.9Hsa8llrabyZvsWoe433Cw_RwRL18aAQaXMDxo";
+    let guild_id = GuildId(
+        u64::from_str(&env::var("GUILD_ID").expect("Expected a guild ID in the environment"))
+            .unwrap(),
+    );
+    let api_key = env::var("API_KEY").expect("Expected an API key in the environment");
+    let token_id = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token_id, intents)
         .event_handler(Bot {
